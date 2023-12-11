1a9e8f098353717f53b0323c92b6657577ab223b
Ajit-Ravan <ravanajit110@gmail.com>
2023-12-11 16:02:21 +0530

    correction in Problem 33 and 34
    33. Find Digits
    34. Circular Array

--- KumarKPractice/src/Step_01_HackerrankQuestions/CircularArrayRotation_Que_34.java
+++ KumarKPractice/src/Step_01_HackerrankQuestions/CircularArrayRotation_Que_34.java
@@ -1,6 +1,9 @@
 package Step_01_HackerrankQuestions;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CircularArrayRotation_Que_34 {
@@ -9,29 +12,47 @@ public class CircularArrayRotation_Que_34 {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 
-        // Reading inputs
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        int q = scanner.nextInt();
+		// Reading inputs
+		int n = scanner.nextInt();
+		int k = scanner.nextInt();
+		int q = scanner.nextInt();
 
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = scanner.nextInt();
-        }
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		int[] queries = new int[q];
+		for (int i = 0; i < q; i++) {
+			queries[i] = scanner.nextInt();
+		}
 
-        // Perform rotation
-        k = k % n; // To handle cases where k > n, reducing unnecessary rotations
-        int[] rotatedArray = new int[n];
-        System.arraycopy(array, n - k, rotatedArray, 0, k);
-        System.arraycopy(array, 0, rotatedArray, k, n - k);
+//        // Perform rotation
+//        k = k % n; // To handle cases where k > n, reducing unnecessary rotations
+//        int[] rotatedArray = new int[n];
+//        System.arraycopy(array, n - k, rotatedArray, 0, k);
+//        System.arraycopy(array, 0, rotatedArray, k, n - k);
+//
+//        // Retrieve elements at specified indices
+//        for (int i = 0; i < q; i++) {
+//            int query = scanner.nextInt();
+//            System.out.println(rotatedArray[query]);
+//        }
+//
+//        scanner.close();
 
-        // Retrieve elements at specified indices
-        for (int i = 0; i < q; i++) {
-            int query = scanner.nextInt();
-            System.out.println(rotatedArray[query]);
-        }
+		// 2nd solution
+		int er = k%arr.length;
+		Map<Integer,Integer> hmap = new HashMap<>();
+			for (int i = 0; i < arr.length; i++) {
+				hmap.put((i+er)%arr.length, arr[i]);
+			}
 
-        scanner.close();
+		List<Integer>list = new ArrayList<>();
+		for(Integer r: queries) {
+			list.add(hmap.get(r));
+			System.out.println(hmap.get(r));
+		}
+
 
 	}
 
--- KumarKPractice/src/Step_01_HackerrankQuestions/FindDigits_Que_33.java
+++ KumarKPractice/src/Step_01_HackerrankQuestions/FindDigits_Que_33.java
@@ -7,29 +7,18 @@ public class FindDigits_Que_33 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int count = 0;
 		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-		}
-
-		for (int i = 0; i < arr.length; i++) {
-			int num = arr[i];
-			int r = 0;
-			while (num != 0) {
-				r = num % 10;
-				if (r == 0) {
-					count--;
-					break;
-				}
-				if (num % r == 0)
-					count++;
-				num = num / 10;
+		int temp = n;
+		int count = 0;
+		while (temp != 0) {
+			int rem = temp % 10;
+			if (rem != 0 && n % rem == 0) {
+				count++;
 			}
+			temp/=10;
 			System.out.println(count);
-		}
 
+		}
 	}
 
 }
