5b9ac75f201e4d4a1aa95cae703c7bfdd73ac7dc
Ajit-Ravan <ravanajit110@gmail.com>
2023-12-26 22:41:32 +0530

    STEP4 session3
    Que 01 -Longest Consecutive Sequence

---
+++ KumarKPractice/src/Step_04_Session_03/Que_01_LongestConsecutiveSequence.java
@@ -0,0 +1,61 @@
+package Step_04_Session_03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Que_01_LongestConsecutiveSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int[] nums = new int[n];
+//		for (int i = 0; i < nums.length; i++) {
+//			nums[i] = sc.nextInt();
+//		}
+//		Arrays.sort(nums);
+//		List<Integer> list = new ArrayList<>();
+//		for (int i = 0; i < nums.length-1; i++) {
+//			if (i != nums.length-1) {
+//				if ((nums[i] + 1) == nums[i + 1]) {
+//					list.add(nums[i]);
+//				}
+//			}
+//		}
+//		System.out.println(list);
+		
+		
+//		SOLUTION
+		
+//		if(nums == null || nums.length == 0)
+//            return 0;
+//
+//        HashSet<Integer> set = new HashSet<>();
+//        for(int num : nums)
+//            set.add(num);
+//
+//        int maxLength = 0;
+//
+//        for(int num : nums) {
+//            if(!set.contains(num - 1)) { // Start of a sequence
+//                int currentNum = num;
+//                int currentLength = 1;
+//
+//                while(set.contains(currentNum + 1)) {
+//                    currentNum++;
+//                    currentLength++;
+//                }
+//
+//                maxLength = Math.max(maxLength, currentLength);
+//            }
+//        }
+//
+//        return maxLength;
+		
+		
+
+	}
+
+}
