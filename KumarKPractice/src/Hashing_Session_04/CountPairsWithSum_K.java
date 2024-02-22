5baccd5d33270b27194c3697461d9a952dc55dea
Ajit-Ravan <ravanajit110@gmail.com>
2024-02-22 19:02:58 +0530

    Hashing count pairs with sum k _ Hashmap

---
+++ KumarKPractice/src/Hashing_Session_04/CountPairsWithSum_K.java
@@ -0,0 +1,28 @@
+package Hashing_Session_04;
+
+import java.util.HashMap;
+//
+//Input: nums = [1,2,2,1], k = 1
+//Output: 4
+
+public class CountPairsWithSum_K {
+	 public static int countKDifference(int[] nums, int k) {
+	        HashMap<Integer, Integer> f = new HashMap<Integer, Integer>();
+	        int res = 0;
+	        for (int i : nums) {
+	            res += f.getOrDefault(i - k, 0) + f.getOrDefault(i + k, 0);
+	            f.put(i, f.getOrDefault(i, 0) + 1);
+	        }
+	        return res;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] nums = new int [] {1,2,2,1};
+		int k=1;
+		int ans = countKDifference(nums,k);
+		System.out.println(ans);
+
+	}
+
+}
